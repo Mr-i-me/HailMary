478b10ba238f483ce0201a593807f89d7b6047ef
lucas souza <ibmstemp@gmail.com>
2021-06-14 18:57:37 -0300

    fixup

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,11 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import Age from '../pages/Age';
 import Home from '../pages/Home';
+import Store from '../pages/Store';
+import Tecido from '../pages/Tecido';
+import StoreItem from '../pages/StoreItem'
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Age} />
     <Route exact path="/home" component={Home} />
+    <Route exact path="/store" component={Store} />
+    <Route exact path="/tecido" component={Tecido} />
+    <Route exact path="/Item" component={StoreItem} />
   </Switch>
 );
 
