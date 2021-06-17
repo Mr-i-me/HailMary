74dc829cdccbf706f1304d254b1d487649331028
lucas souza <ibmstemp@gmail.com>
2021-06-16 21:59:12 -0300

    add carousel to home page

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Age from '../pages/Age';
 import Home from '../pages/Home';
-import Store from '../pages/Store';
-import Tecido from '../pages/Tecido_';
-import StoreItem from '../pages/StoreItem'
-import About from '../pages/About'
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Age} />
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/store" component={Store} />
-    <Route exact path="/tecido" component={Tecido} />
-    <Route exact path="/Item" component={StoreItem} />
-    <Route exact path="/Sobre" component={About} />
-  </Switch>
+    <Switch>
+        <Route exact path="/" component={Age}/>
+        <Route exact path="/home" component={Home}/>
+    </Switch>
 );
 
 export default Routes;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Example from "./Carousel/Example";
-import {Nav, Navbar, NavbarBrand} from "reactstrap";
-import {Link} from "react-router-dom";
 import Navigation from "../../components/Navigation";
-// import {Carousel} from "reactstrap";
 
 
 
