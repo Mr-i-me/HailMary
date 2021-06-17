623ef4733083eddab374846749b988562248af3b
lucas souza <ibmstemp@gmail.com>
2021-06-16 22:14:18 -0300

    feat: add carousel to homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Example from "./Carousel/Example";
+import {Nav, Navbar, NavbarBrand} from "reactstrap";
+import {Link} from "react-router-dom";
+import Navigation from "../../components/Navigation";
+// import {Carousel} from "reactstrap";
 
-const Home = (props) => (
-    <div>
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-      </ul>
-    </div>
+
+
+const Home = props => (
+    <>
+        <div>
+            <Navigation/>
+        </div>
+        <section className="Home">
+            <div>
+                <Example/>
+            </div>
+        </section>
+    </>
 );
 
 export default Home;
